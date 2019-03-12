38e378c28c4730dbe7ea2939116db52556bf364a
Sergey Stroev <stainlessrat83@gmail.com>
2019-03-12 14:00:17 +0700

    get String Array and substring: String Advanced

--- stringadvanced/src/main/java/rezept_day/ucoz/ru/stringadvanced/MainActivity.java
+++ stringadvanced/src/main/java/rezept_day/ucoz/ru/stringadvanced/MainActivity.java
@@ -2,6 +2,7 @@ package rezept_day.ucoz.ru.stringadvanced;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        String nameString = "Андрей, Алексей, Роман, Павел, Михаил";
+//        String[] names = nameString.split(", ");//Разбиваем строку по именам, использую для разделения символ запятой и пробел
+//        for (String name: names){
+//            Log.i("MyName", name);
+//        }
+        //Как получить часть строки
+        String geometry = "Геометрия";
+        String meter = geometry.substring(3, 7);
+        Log.i("MyName", meter);
     }
 }
