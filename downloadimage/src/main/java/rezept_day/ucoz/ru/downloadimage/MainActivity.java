429a2440e9a4070f4219179f3abde7262412843e
Sergey Stroev <stainlessrat83@gmail.com>
2019-03-12 13:16:17 +0700

    ready: Download Image

--- downloadimage/src/main/java/rezept_day/ucoz/ru/downloadimage/MainActivity.java
+++ downloadimage/src/main/java/rezept_day/ucoz/ru/downloadimage/MainActivity.java
@@ -1,20 +1,39 @@
 package rezept_day.ucoz.ru.downloadimage;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ImageView imageView;
+    private String url = "http://images.eil.com/large_image/ALIZEE_MADEMOISELLE%2BJULIETTE-424396.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
     }
 
     public void onClickDownloadImage(View view) {
+        DownloadImageTask task = new DownloadImageTask();
+        Bitmap bitmap = null;
+        try {
+            bitmap = task.execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
