ceb5ef310c4e07ee401551cda1baf3c3bed8c5a6
Sergey Stroev <stainlessrat83@gmail.com>
2019-03-12 14:36:15 +0700

    pattern, compile, matcher: String Advanced

--- stringadvanced/src/main/java/rezept_day/ucoz/ru/stringadvanced/MainActivity.java
+++ stringadvanced/src/main/java/rezept_day/ucoz/ru/stringadvanced/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,8 +20,19 @@ public class MainActivity extends AppCompatActivity {
 //            Log.i("MyName", name);
 //        }
         //Как получить часть строки
-        String geometry = "Геометрия";
-        String meter = geometry.substring(3, 7);
-        Log.i("MyName", meter);
+//        String geometry = "Геометрия";
+//        String meter = geometry.substring(3, 7);
+//        Log.i("MyName", meter);
+
+        // Используем регулярные  выражения
+        String river = "Mississippi";
+        //Создаем шаблон
+        Pattern pattern = Pattern.compile("Mi(.*?)pi");//Найди строчку которая начинается на Mi и заканчивается на pi, и верни все что находится между.
+        //Чтобы обработать наш паттерн, нам нужен объект Matcher
+        Matcher matcher = pattern.matcher(river);
+        //matcher.find() - ищет первое вхождение паттерна
+        while (matcher.find()){
+            Log.i("MyName", matcher.group(1));//чтобы вызвать первое совпадение используем matcher.group(1)
+        }
     }
 }
