a776c8c2cc1f1bbfc3bd150857157d95b4e3e923
Sergey Stroev <stainlessrat83@gmail.com>
2019-03-12 11:11:49 +0700

    using Log

--- app/src/main/java/rezept_day/ucoz/ru/downloadwebcontent/MainActivity.java
+++ app/src/main/java/rezept_day/ucoz/ru/downloadwebcontent/MainActivity.java
@@ -2,12 +2,17 @@ package rezept_day.ucoz.ru.downloadwebcontent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mailRu = "https://mail.ru/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Hello", "Hello, world!");
     }
 }
